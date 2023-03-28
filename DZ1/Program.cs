84e3e008b7d5ebc952ae7d93adc540cd1e534dc0
Alexandr <sasha-bragin@mail.ru>
2023-03-29 01:19:48 +0300

    Аккерман нехороший

---
+++ DZ1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт 
+// сумму натуральных элементов в 
+// промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int m= 4;
+int n= 8;
+int RecNumbers (int m, int n)
+{
+    int sum=m;
+    if(m==n)
+    {
+    return 0;
+    }else
+    {
+        m++;
+        sum = m + RecNumbers(m,n);
+        return sum;
+    }
+
+}
+
+Console.WriteLine(RecNumbers(m - 1,n));
+
+
+// 1 55
